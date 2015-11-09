a9a96d49bd37250149b79a6ffd86b95dca71b2bb
Flurry2010 <flurry2010@gmail.com>
2015-11-09 13:01:28 +0100

    Initial commit

---
+++ PHP/Einstieg/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?PHP
+    echo "<title>PHP</title>";
+    ?>
+</head>
+<body>
+
+
+<!-- PHP Ausgabe über echo -->
+<?php
+echo "Hallo du Affe!!\n<br>";
+echo "Bisse noch Voll?";
+
+$titel = "Schwein";
+$preis = '€ 9.50';
+echo "Das Kilo $titel kostet $preis.<br>";
+echo 'Das Kilo $titel kostet ' . $preis . "<br>";
+echo 'Das Kilo $titel kostet $preis.<br>';
+echo "'' und \$ <br>";
+$tierliste = array("tier" => "Pferd");
+echo "Das Kilo {$tierliste['tier']} kostet $preis.<br>";
+?>
+
+<!-- PHP Schleifen Übung -->
+<?php
+
+$header = "Kapitel 1";
+echo "<h1>$header</h1>";
+
+
+    for ($i = 1; $i < 10; $i++) {
+        if ($i % 2 == 1) {
+            echo "<span style= color:red>";
+        }
+        for ($j = 1; $j < $i; $j++) {
+            echo "o";
+        }
+        if (($i % 2) == 1) {
+            echo "</span>";
+        }
+        echo "<br>";
+    }
+
+    for ($i = 10; $i >= 1; $i--) {
+        if ($i % 2 == 1) {
+            echo "<span style= color:red>";
+        }
+        for ($j = $i; $j > 0; $j--) {
+            echo "o";
+        }
+        if (($i % 2) == 1) {
+            echo "</span>";
+        }
+        echo "<br>";
+    }
+
+?>
+
+<!-- PHP Schleifen Übung optimiert -->
+<?php
+$header = "Kapitel 1";
+echo "<h1>$header</h1>";
+
+for ($h = 0; $h < 14; $h++) {
+    if ($h % 2 == 0)
+        echo "<div style='text-align: left;vertical-align: top;display: inline-block'>";
+    else echo "<div style='text-align: right;vertical-align: top;display: inline-block'>";
+    for ($i = 1; $i <= 16; $i++) {
+        if ($i % 2 == 0) echo "<div style='color: red'>";
+        else echo "<div style='color: black'>";
+        for ($j = $i; $j > 0; $j--) echo "o";
+        echo "&nbsp;";
+        echo "</div>";
+    }
+    for ($i = 15; $i >= 1; $i--) {
+        if ($i % 2 == 0) echo "<div style='color: red'>";
+        else echo "<div style='color: black'>";
+        for ($j = $i; $j > 0; $j--) echo "o";
+        echo "&nbsp;";
+        echo "</div>";
+    }
+    echo "</div>";
+}
+?>
+
+<?PHP
+$array = array("Alfred" => array("Audi" => array ("Farbe" => "Lila","PS" => "180", "Alter" => "12 Jahre" )),
+"Ingrid" => array ("VW Käfer" => array ("Farbe" => "gelb","PS" => "110", "Alter" => "6 Jahre" )),
+"Lisa" => array ("VW Lupo" => array("Farbe" => "Rot","PS" => "90", "Alter" => "12 Jahre" )));
+
+echo "<ul>";
+foreach ($array as $key => $value) {
+   echo "$key";
+    echo "<ol><li>";
+    foreach ($value as $key => $value) {
+        echo "$key";
+        echo "<ul>";
+        foreach ($value as $key => $value) {
+            echo "<li> $key : $value</li>";
+        }
+        echo "</ul></li>";
+    }
+    echo "</ol>";
+}echo "</ul>";
+
+?>
+
+
+</body>
+</html>
---
+++ PHP/index.php
@@ -0,0 +1,20 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dfleuren
+ * Date: 04.11.2015
+ * Time: 07:44
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+
+
+</body>
+</html>
