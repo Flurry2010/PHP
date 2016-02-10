ea0282fe99c0f3aec4cc69eaff19bcfa48f2d4b3
Flurry2010 <flurry2010@gmail.com>
2016-02-10 09:51:52 +0100

    Übung

--- Script/Event/farbtabelle/farbTabelle.js
+++ Script/Event/farbtabelle/farbTabelle.js
@@ -4,4 +4,45 @@
 
 "use strict";
 
+var tbody = document.getElementsByTagName('tbody')[0];
+
+var content = '';
+
+for (let i = 0; i < 16; i++) {
+    content += '<tr>';
+    for (let i = 0; i < 16; i++) {
+        content += '<td></td>';
+    }
+
+    content += '</tr>';
+}
+
+tbody.innerHTML = content;
+
+var tds = document.getElementsByTagName('td');
+var radio = document.querySelectorAll('input');
+
+for (let i = 0; i < 256; i++) {
+    tds[i].style.backgroundColor = 'rgb(' + i + ',0,0)';
+}
+
+for (let i = 0; i < radio.length; i++)
+    radio[i].addEventListener('click', function () {
+
+      if(radio[i].id == "red") {
+          for (let i = 0; i < 256; i++) {
+              tds[i].style.backgroundColor = 'rgb(' + i + ',0,0)';
+          }
+      }else if(radio[i].id == "green") {
+              for (let i = 0; i < 256; i++) {
+                  tds[i].style.backgroundColor = 'rgb(0, ' + i + ',0)';
+              }
+          }else if(radio[i].id == "blue") {
+    for (let i = 0; i < 256; i++) {
+        tds[i].style.backgroundColor = 'rgb(0,0,' + i + ')';
+    }
+}
+    });
+
+
 
