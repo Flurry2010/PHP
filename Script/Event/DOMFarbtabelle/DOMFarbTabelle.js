9f1f374c9d0686594f4301945e1f145903b9ce01
Flurry2010 <flurry2010@gmail.com>
2016-02-24 08:29:17 +0100

    Übung

---
+++ Script/Event/DOMFarbtabelle/DOMFarbTabelle.js
@@ -0,0 +1,46 @@
+/**
+ * Created by dfleuren on 24.02.2016.
+ */
+"use strict";
+
+var tbody = document.getElementsByTagName('tbody')[0];
+
+var content = '';
+
+for (let i = 0; i < 16; i++) {
+    let tr = document.createElement('tr');
+    for (let i = 0; i < 16; i++) {
+        let td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+}
+
+
+var tds = document.getElementsByTagName('td');
+var radio = document.querySelectorAll('input');
+
+for (let i = 0; i < 256; i++) {
+    tds[i].style.backgroundColor = 'rgb(' + i + ',0,0)';
+}
+
+for (let i = 0; i < radio.length; i++)
+    radio[i].addEventListener('click', function () {
+
+        if(radio[i].id == "red") {
+            for (let i = 0; i < 256; i++) {
+                tds[i].style.backgroundColor = 'rgb(' + i + ',0,0)';
+            }
+        }else if(radio[i].id == "green") {
+            for (let i = 0; i < 256; i++) {
+                tds[i].style.backgroundColor = 'rgb(0, ' + i + ',0)';
+            }
+        }else if(radio[i].id == "blue") {
+            for (let i = 0; i < 256; i++) {
+                tds[i].style.backgroundColor = 'rgb(0,0,' + i + ')';
+            }
+        }
+    });
+
+
+
