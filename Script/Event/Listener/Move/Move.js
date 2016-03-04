75a8b02591db8925ee7d281480ff3401e78e6c45
Flurry2010 <flurry2010@gmail.com>
2016-03-04 08:44:58 +0100

    Übung

--- Script/Event/Listener/Move/Move.js
+++ Script/Event/Listener/Move/Move.js
@@ -5,9 +5,44 @@
 
 
 var container = document.querySelector('#container');
+
 container.addEventListener('mousedown', function (e) {
 
-    if (e.target == container) return;
+    if (e.target === container) return;
+
+    let mx = e.clientX;
+    let my = e.clientY;
+
+    let ex = e.target.offsetLeft;
+    let ey = e.target.offsetTop;
+
+    let target = e.target;
 
 
-});
+    var mousemove = function(e) {
+
+            let left = ex - mx + e.clientX;
+            let top = ey - my + e.clientY;
+
+            if (left > 380) left = 380;
+            if (left < 0) left = 0;
+            if (top > 380) top = 380;
+            if (top < 0) top = 0;
+
+            target.style.left = left + 'px';
+            target.style.top = top + 'px';
+
+        };
+
+    var mouseup = function(e) {
+            document.removeEventListener('mousemove', mousemove);
+            document.removeEventListener('mouseup', mouseup);
+        };
+
+    document.addEventListener('mousemove', mousemove);
+    document.addEventListener('mouseup', mouseup);
+
+    });
+
+    div.addEventListener('mousedown', mousedown);
+
